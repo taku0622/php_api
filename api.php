9883bceabe152150906af8fe9e9ad0d38c1bc67d
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-10-18 23:33:44 +0900

    done!

--- api.php
+++ api.php
@@ -2,7 +2,7 @@
 // 文字コード設定
 header('Content-Type: application/json; charset=UTF-8');
 
-$param = htmlspecialchars($_GET["text"]);
+$param = htmlspecialchars($_GET["/api.php"]);
 if ($param == "info") {
   $arr["text"] = "新着情報はこちらです";
 } else {
