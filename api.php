3f29400ca93c69888fc48ddd19a547db229c352b
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-10-19 22:29:41 +0900

    done

--- api.php
+++ api.php
@@ -8,6 +8,9 @@ header('Content-Type: application/json; charset=UTF-8');
 // POSTされたJSON文字列を取り出し
 $json = file_get_contents("php://input");
 
+if (!empty($json)) {
+  error_log("json is exist");
+}
 // JSON文字列をobjectに変換
 //   ⇒ 第2引数をtrueにしないとハマるので注意
 // $contents = json_decode($json);
