272a43ea942692ccf3eb0493b2671de3f55b9707
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-10-24 09:46:22 +0900

    done

--- api.php
+++ api.php
@@ -60,23 +60,23 @@ function post($object)
   // 送信の終了
   curl_close($curl);
 
-  $headers = array(
-    "Content-Type: application/json",
-  );
-  $curl = curl_init("https://chatbot-1015-b.herokuapp.com/index.php");
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-  curl_setopt($curl, CURLOPT_POST, true);
-  curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
-  curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-  // 送信の実行
-  $result2 = curl_exec($curl);
-  // 送信の終了
-  curl_close($curl);
+  // $headers = array(
+  //   "Content-Type: application/json",
+  // );
+  // $curl = curl_init("https://chatbot-1015-b.herokuapp.com/index.php");
+  // curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+  // curl_setopt($curl, CURLOPT_POST, true);
+  // curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
+  // curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+  // // 送信の実行
+  // $result2 = curl_exec($curl);
+  // // 送信の終了
+  // curl_close($curl);
 
-  header("Content-Type: text/javascript; charset=utf-8");
-  echo json_encode($object, JSON_UNESCAPED_UNICODE); // 配列をJSON形式に変換してくれる
-  error_log("done echo!!!!");
-  // exit();
+  // header("Content-Type: text/javascript; charset=utf-8");
+  // echo json_encode($object, JSON_UNESCAPED_UNICODE); // 配列をJSON形式に変換してくれる
+  // error_log("done echo!!!!");
+  // // exit();
 
   ################################################
   // $url = 'https://tut-line-bot-test.glitch.me/push';
