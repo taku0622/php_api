3ed07b7867f8b5c984e28e7bb78c79f239125740
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-10-24 23:09:45 +0900

    done

--- data.php
+++ data.php
@@ -224,6 +224,7 @@ function cancel_info()
 function event_info()
 {
   $messages = [
+    [
     "type": "text",
     "text": "今後のイベントはこのようになっています\n
     11/03(火):祝日授業開講\n
@@ -231,6 +232,7 @@ function event_info()
     11/23(月):祝日授業開講\n
     12月中旬:後期末試験時間割発表\n
     12/26(土)~01/04(月):冬期休業\n"
+    ]
   ];
   return $messages;
 }
