27d7ec4968df7bdf70337b3adb4b83c131625f90
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-10-25 17:10:45 +0900

    done

--- data.php
+++ data.php
@@ -102,58 +102,92 @@ function important_info()
   $messages = [
     [
       'type' => 'template',
-      'altText' => 'イベント',
+      'altText' => '重要情報',
       'template' => [
         'type' => 'carousel',
         'columns' => [
           #----------------------------------------1---------------------------------------------
           [
-            'title' => '『学内ミニ合同企業説明会』の開催について',
-            'text' => '【対象:2020年3月卒業･修了予定者】',
+            'title' => '2020年度後期　履修に関する掲示一覧(八王子キャンパス)',
+            'text' => '履修に関する掲示一覧(八王子キャンパス)',
             'defaultAction' => [
               'type' => 'uri',
               'label' => 'View detail',
-              'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/76553/'
+              'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/2018/'
             ],
             'actions' => [
               [
                 'type' => 'uri',
                 'label' => '詳細',
-                'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/76553/'
+                'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/2018/'
               ]
             ]
           ],
           #----------------------------------------2---------------------------------------------
           [
-            'title' => 'クリエイティブ業界学内合同企業セミナーのお知らせ',
-            'text' => '学内合同企業セミナーを実施します',
+            'title' => '2020年度後期 遠隔システム（Zoom）による相談受け付けについて',
+            'text' => '前期に引き続き後期も遠隔システム（Zoom）で相談を受け付けます。',
             'defaultAction' => [
               'type' => 'uri',
               'label' => 'View detail',
-              'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/35103/'
+              'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/93230/'
             ],
             'actions' => [
               [
                 'type' => 'uri',
                 'label' => '詳細',
-                'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/35103/'
+                'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/93230/'
               ]
             ]
           ],
           #----------------------------------------3---------------------------------------------
           [
-            'title' => '大林組グループ会社説明会』の開催について',
-            'text' => '対象:学部3年生および大学院修士1年生／学部不問】',
+            'title' => '【八王子みなみ野駅】行きスクールバス発着所の一時変更について',
+            'text' => '【八王子みなみ野駅】行きスクールバス発着所を一時変更いたしますので、ご確認ください。',
             'defaultAction' => [
               'type' => 'uri',
               'label' => 'View detail',
-              'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/53517/'
+              'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/95242/'
             ],
             'actions' => [
               [
                 'type' => 'uri',
                 'label' => '詳細',
-                'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/53517/'
+                'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/95242/'
+              ]
+            ]
+          ],
+          #----------------------------------------4---------------------------------------------
+          [
+            'title' => 'オンライン大学祭の開催について',
+            'text' => '今年度は新型コロナウイルスの影響',
+            'defaultAction' => [
+              'type' => 'uri',
+              'label' => 'View detail',
+              'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/95052/'
+            ],
+            'actions' => [
+              [
+                'type' => 'uri',
+                'label' => '詳細',
+                'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/95052/'
+              ]
+            ]
+          ],
+          #----------------------------------------5---------------------------------------------
+          [
+            'title' => '【追加募集】令和2年度日本学生支援',
+            'text' => '日本学生支援機構から給付型奨学金及び第二種奨学金',
+            'defaultAction' => [
+              'type' => 'uri',
+              'label' => 'View detail',
+              'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/95044/'
+            ],
+            'actions' => [
+              [
+                'type' => 'uri',
+                'label' => '詳細',
+                'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/95044/'
               ]
             ]
           ]
@@ -240,87 +274,58 @@ function event_info()
   $messages = [
     [
       'type' => 'template',
-      'altText' => '新着情報',
+      'altText' => 'イベント',
       'template' => [
         'type' => 'carousel',
         'columns' => [
+          #----------------------------------------1---------------------------------------------
           [
-            'title' => '【図書館】リクエストの結果報告＜八王子キャンパス＞',
-            'text' => '10月（前半）の選書の結果、以下のリクエストが採択されました。',
+            'title' => '『学内ミニ合同企業説明会』の開催について',
+            'text' => '【対象:2020年3月卒業･修了予定者】',
             'defaultAction' => [
               'type' => 'uri',
               'label' => 'View detail',
-              'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/64555/'
+              'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/76553/'
             ],
             'actions' => [
               [
                 'type' => 'uri',
                 'label' => '詳細',
-                'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/64555/'
+                'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/76553/'
               ]
             ]
           ],
+          #----------------------------------------2---------------------------------------------
           [
-            'title' => '2020年度第2学期（後期）放送大学特別聴講学生',
-            'text' => '放送大学特別聴講学生へ',
+            'title' => 'クリエイティブ業界学内合同企業セミナーのお知らせ',
+            'text' => '学内合同企業セミナーを実施します',
             'defaultAction' => [
               'type' => 'uri',
               'label' => 'View detail',
-              'uri' => 'https://service.cloud.teu.ac.jp/inside2/wp-content/uploads/2020/10/2020_dai2gakki_housoudaigaku_1022.pdf'
+              'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/35103/'
             ],
             'actions' => [
               [
                 'type' => 'uri',
                 'label' => '詳細',
-                'uri' => 'https://service.cloud.teu.ac.jp/inside2/wp-content/uploads/2020/10/2020_dai2gakki_housoudaigaku_1022.pdf'
+                'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/35103/'
               ]
             ]
           ],
+          #----------------------------------------3---------------------------------------------
           [
-            'title' => '【CS学部】2020年度「創成課題」教室（10/22更新）',
-            'text' => '属された研究室ごとに、創成課題を行います。',
+            'title' => '大林組グループ会社説明会』の開催について',
+            'text' => '対象:学部3年生および大学院修士1年生／学部不問】',
             'defaultAction' => [
               'type' => 'uri',
               'label' => 'View detail',
-              'uri' => 'https://service.cloud.teu.ac.jp/inside2/wp-content/uploads/2020/10/2020CS_souseikadai_kyousitu20201022.pdf'
+              'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/53517/'
             ],
             'actions' => [
               [
                 'type' => 'uri',
                 'label' => '詳細',
-                'uri' => 'https://service.cloud.teu.ac.jp/inside2/wp-content/uploads/2020/10/2020CS_souseikadai_kyousitu20201022.pdf'
-              ]
-            ]
-          ],
-          [
-            'title' => 'シェアサイクル設置のお知らせ（八王子キャンパス）',
-            'text' => '八王子キャンパスにシェアサイクルを設置することになりました。',
-            'defaultAction' => [
-              'type' => 'uri',
-              'label' => 'View detail',
-              'uri' => 'https://service.cloud.teu.ac.jp/inside2/wp-content/uploads/2020/10/shearingu_settiosirase_1021.pdf'
-            ],
-            'actions' => [
-              [
-                'type' => 'uri',
-                'label' => '詳細',
-                'uri' => 'https://service.cloud.teu.ac.jp/inside2/wp-content/uploads/2020/10/shearingu_settiosirase_1021.pdf'
-              ]
-            ]
-          ],
-          [
-            'title' => '【図書館】図書館アルバイトを募集します！＜八王子キャンパス＞',
-            'text' => 'お申し込みを お待ちしています。',
-            'defaultAction' => [
-              'type' => 'uri',
-              'label' => 'View detail',
-              'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/12658/'
-            ],
-            'actions' => [
-              [
-                'type' => 'uri',
-                'label' => '詳細',
-                'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/12658/'
+                'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/53517/'
               ]
             ]
           ]
