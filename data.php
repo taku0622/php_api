f542fb8456ed877a2b822b6f18ace64a518439ff
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-10-25 14:19:45 +0900

    done

--- data.php
+++ data.php
@@ -117,56 +117,88 @@ function important_info()
           [
             'thumbnailImageUrl' => 'https://example.com/bot/images/item1.jpg',
             'imageBackgroundColor' => '#FFFFFF',
-            'title' => '重要情報1',
-            'text' => 'description',
+            'title' => '2020年度後期　履修に関する掲示一覧(八王子キャンパス)',
+            'text' => '履修に関する掲示一覧(八王子キャンパス)',
             'defaultAction' => [
               'type' => 'uri',
               'label' => 'View detail',
-              'uri' => 'http://example.com/page/123'
+              'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/2018/'
             ],
             'actions' => [
               [
-                'type' => 'postback',
-                'label' => 'Buy',
-                'data' => 'action=buy&itemid=111'
-              ],
-              [
-                'type' => 'postback',
-                'label' => 'Add to cart',
-                'data' => 'action=buy&itemid=111'
-              ],
-              [
-                'type' => 'postback',
-                'label' => 'View detail',
-                'data' => 'action=buy&itemid=111'
+                'type' => 'uri',
+                'label' => '詳細',
+                'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/2018/'
               ]
             ]
           ],
           [
-            'thumbnailImageUrl' => 'https://example.com/bot/images/item2.jpg',
-            'imageBackgroundColor' => '#000000',
-            'title' => '重要情報2',
-            'text' => 'description',
+            'thumbnailImageUrl' => 'https://example.com/bot/images/item1.jpg',
+            'imageBackgroundColor' => '#FFFFFF',
+            'title' => '2020年度第2学期（後期）放送大学特別聴講学生',
+            'text' => '放送大学特別聴講学生へ',
             'defaultAction' => [
               'type' => 'uri',
               'label' => 'View detail',
-              'uri' => 'http://example.com/page/222'
+              'uri' => 'https://service.cloud.teu.ac.jp/inside2/wp-content/uploads/2020/10/2020_dai2gakki_housoudaigaku_1022.pdf'
             ],
             'actions' => [
               [
-                'type' => 'postback',
-                'label' => 'Buy',
-                'data' => 'action=buy&itemid=222'
-              ],
+                'type' => 'uri',
+                'label' => '詳細',
+                'uri' => 'https://service.cloud.teu.ac.jp/inside2/wp-content/uploads/2020/10/2020_dai2gakki_housoudaigaku_1022.pdf'
+              ]
+            ]
+          ],
+          [
+            'thumbnailImageUrl' => 'https://example.com/bot/images/item1.jpg',
+            'imageBackgroundColor' => '#FFFFFF',
+            'title' => '【CS学部】2020年度「創成課題」教室（10/22更新）',
+            'text' => '属された研究室ごとに、創成課題を行います。',
+            'defaultAction' => [
+              'type' => 'uri',
+              'label' => 'View detail',
+              'uri' => 'https://service.cloud.teu.ac.jp/inside2/wp-content/uploads/2020/10/2020CS_souseikadai_kyousitu20201022.pdf'
+            ],
+            'actions' => [
               [
-                'type' => 'postback',
-                'label' => 'Add to cart',
-                'data' => 'action=buy&itemid=222'
-              ],
+                'type' => 'uri',
+                'label' => '詳細',
+                'uri' => 'https://service.cloud.teu.ac.jp/inside2/wp-content/uploads/2020/10/2020CS_souseikadai_kyousitu20201022.pdf'
+              ]
+            ]
+          ], [
+            'thumbnailImageUrl' => 'https://example.com/bot/images/item1.jpg',
+            'imageBackgroundColor' => '#FFFFFF',
+            'title' => 'シェアサイクル設置のお知らせ（八王子キャンパス）',
+            'text' => '八王子キャンパスにシェアサイクルを設置することになりました。',
+            'defaultAction' => [
+              'type' => 'uri',
+              'label' => 'View detail',
+              'uri' => 'https://service.cloud.teu.ac.jp/inside2/wp-content/uploads/2020/10/shearingu_settiosirase_1021.pdf'
+            ],
+            'actions' => [
               [
-                'type' => 'postback',
-                'label' => 'View detail',
-                'data' => 'action=buy&itemid=222'
+                'type' => 'uri',
+                'label' => '詳細',
+                'uri' => 'https://service.cloud.teu.ac.jp/inside2/wp-content/uploads/2020/10/shearingu_settiosirase_1021.pdf'
+              ]
+            ]
+          ], [
+            'thumbnailImageUrl' => 'https://example.com/bot/images/item1.jpg',
+            'imageBackgroundColor' => '#FFFFFF',
+            'title' => '【図書館】図書館アルバイトを募集します！＜八王子キャンパス＞',
+            'text' => 'お申し込みを お待ちしています。',
+            'defaultAction' => [
+              'type' => 'uri',
+              'label' => 'View detail',
+              'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/12658/'
+            ],
+            'actions' => [
+              [
+                'type' => 'uri',
+                'label' => '詳細',
+                'uri' => 'https://service.cloud.teu.ac.jp/inside2/archives/12658/'
               ]
             ]
           ]
