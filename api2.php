a58b0a1507bb847be9ea0c8c9e51f1507c7d5859
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-01 14:55:36 +0900

    done

--- api2.php
+++ api2.php
@@ -28,7 +28,7 @@ $options = array(
 );
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: x-www-form-urlencoded'));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($ch, CURLOPT_VERBOSE, true);
