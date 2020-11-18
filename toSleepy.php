7e7bcb7a72243d8d4eadf38f21cc9c457f03d3b0
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-11-18 17:19:39 +0900

    done

---
+++ toSleepy.php
@@ -0,0 +1,47 @@
+<?php
+$object = [
+  "id" => "U6e0f4008a090ff5b5bef0323cae3428e",
+  "number" => "B0118276",
+  "push_new" => false,
+  "push_important" => false,
+  "push_cancel" => true,
+  "push_event" => true,
+];
+
+
+// heroku logに表示
+error_log("########################## push important info is ##########################");
+error_log(json_encode($object, JSON_UNESCAPED_UNICODE));
+
+// JSON形式への変換
+// echo json_encode($object, JSON_UNESCAPED_UNICODE);
+
+// JSON形式への変換
+$json =  json_encode([$object], JSON_UNESCAPED_UNICODE);
+
+//curl実行
+$url = "https://sleepy-hamlet-66660.herokuapp.com/api/insert/students";
+$options = array(
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_AUTOREFERER => true,
+);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($ch, CURLOPT_VERBOSE, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt_array($ch, $options);
+// $result = curl_exec($ch);
+$response = curl_exec($ch);
+
+// $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+// $header = substr($response, 0, $header_size);
+// $body = substr($response, $header_size);
+// $result = json_decode($body, true);
+curl_close($ch);
+// return $result;
+echo $response;
